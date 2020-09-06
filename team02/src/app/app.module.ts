72091ee8e9b65e9e885a7f92998e402946c49013
Shan Hu <shan.hu@hva.nl>
2020-09-06 19:03:11 +0200

    laatste fix hopelijk

--- team02/src/app/app.module.ts
+++ team02/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/mainpage/header/header.component';
 import { HomeComponent } from './components/mainpage/home/home.component';
-import { NavBarComponent } from './src/app/components/mainpage/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    NavBarComponent
   ],
   imports: [
     BrowserModule,
