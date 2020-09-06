6a4d22d530997be933253a32ced5ca857e7000aa
Shan Hu <shan.hu@hva.nl>
2020-09-06 19:09:59 +0200

    navbar fix

--- team02/src/app/app.module.ts
+++ team02/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/mainpage/header/header.component';
 import { HomeComponent } from './components/mainpage/home/home.component';
+import { NavBarComponent } from './components/mainpage/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
