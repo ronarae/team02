a04f053af57f58afa4635e0ccd5b28af27f26c5d
ronaraederomasrieza <rona.rieza2393@gmail.com>
2020-09-13 17:48:38 +0200

    table edited

--- team02/src/app/components/events/overview1/overview1.component.ts
+++ team02/src/app/components/events/overview1/overview1.component.ts
@@ -8,6 +8,69 @@ import {AEvent, AEventStatus} from '../../../models/a-event';
 })
 export class Overview1Component implements OnInit {
 
+  id = ['20001', '20002', '20003', '20004', '20005', '20006', '20007', '20008', '20009'];
+  title = ['Fantastic Event 0 ',
+    'Fantastic Event 1',
+    'Fantastic Event 2',
+    'Fantastic Event 3',
+    'Fantastic Event 4',
+    'Fantastic Event 5',
+    'Fantastic Event 6',
+    'Fantastic Event 7',
+    'Fantastic Event 8',
+  ];
+  start = [new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+  ];
+  end = [new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+    new Date('4/9/2020'),
+  ];
+  status = ['CANCELLED',
+    'PUBLISHED',
+    'CANCELLED',
+    'CANCELLED',
+    'DRAFT',
+    'CANCELLED',
+    'CANCELLED',
+    'PUBLISHED',
+    'CANCELLED',
+  ];
+  entranceFee = [Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+    Math.floor(Math.random() * 6) * 10,
+  ];
+  maxParticipants = [Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+    Math.floor((Math.random() + 1) * 6) * 100,
+  ];
+
+  IsTicketed = [];
 
   public aEvents: AEvent[];
 
@@ -15,36 +78,31 @@ export class Overview1Component implements OnInit {
   }
 
   // tslint:disable-next-line:typedef
+  public addRandomAEvent(index){
+    this.aEvents.push(AEvent.createRandomAEvent(index));
+  }
+
+
+  // tslint:disable-next-line:typedef
   ngOnInit() {
     this.aEvents = [];
-    for (let i = 0; i < 9; i++) {
-      this.aEvents.push(this.addRandomAEvent(i));
+    for (let i = 0; i < this.id.length; i++) {
+      const event =  new AEvent(
+        this.id[i],
+        this.title[i],
+        this.start[i],
+        this.end[i],
+        this.status[i],
+        this.IsTicketed[i],
+        this.entranceFee[i],
+        this.maxParticipants[i]
+      );
+      console.log(event.id);
+
+      this.aEvents.push(event);
     }
   }
 
-  // tslint:disable-next-line:typedef
-  public addRandomAEvent(index) {
-  const event = new AEvent();
-  event.title = 'Fantastic event ' + index;
-
-  let date = new Date();
-  date.setMonth(date.getMonth() * (index + 1));
-  event.start = date;
-
-  date = new Date();
-  date.setMonth(date.getMonth() * (index + 1));
-  event.end = date;
-
-  const rand = Math.floor(Math.random() * Object.keys(AEventStatus).length);
-  const randomStatus = AEventStatus[Object.keys(AEventStatus)[rand]];
-  event.status = randomStatus;
-
-  event.participationFee = Math.floor(Math.random() * 6) * 10;
-  event.maxParticipants = Math.floor((Math.random() + 1) * 6) * 100;
-
-  console.log('added events');
-  return event;
-  }
 
 }
 
--- team02/src/app/models/a-event.ts
+++ team02/src/app/models/a-event.ts
@@ -1,4 +1,5 @@
 export class AEvent {
+  id: number;
   title: string;
   start: Date;
   end: Date;
@@ -7,13 +8,62 @@ export class AEvent {
 
   // tslint:disable-next-line:ban-types
   IsTicketed: Boolean;
-  participationFee: number;
+  entranceFee: number;
   maxParticipants: number;
 
+  constructor(id, title, start, end, status, IsTicketed, entranceFee, maxParticipants) {
+    this.id = id;
+    this.title = title;
+    this.start = start;
+    this.end = end;
+    this.status = status;
+    this.IsTicketed = IsTicketed;
+    this.entranceFee = entranceFee;
+    this.maxParticipants = maxParticipants;
+  }
+
+  // tslint:disable-next-line:typedef
+  public static createRandomAEvent(index): AEvent {
+    // @ts-ignore
+    const event = new AEvent();
+    event.id = index + 20001;
+    event.title = 'Fantastic event ' + index;
+
+    let date = new Date();
+    date.setMonth(date.getMonth() * (index + 1));
+    event.start = date;
+
+    date = new Date();
+    date.setMonth(date.getMonth() * (index + 1));
+    event.end = date;
+
+    const rand = Math.floor(Math.random() * Object.keys(AEventStatus).length);
+    const randomStatus = AEventStatus[Object.keys(AEventStatus)[rand]];
+    event.status = randomStatus;
+
+    event.entranceFee = Math.floor(Math.random() * 6) * 10;
+    event.maxParticipants = Math.floor((Math.random() + 1) * 6) * 100;
+
+
+    if (event.status === 'PUBLISHED'){
+      event.entranceFee = Math.floor(Math.random() * 6) * 10;
+      event.maxParticipants = Math.floor((Math.random() + 1) * 6) * 100;
+    } else {
+
+      event.entranceFee = parseFloat(' ');
+      event.maxParticipants = parseFloat(' ');
+    }
+    return event;
+
+
+  }
+
 }
+
 
 export enum AEventStatus {
   draft = 'DRAFT',
   published = 'PUBLISHED',
   cancelled = 'CANCELLED'
 }
+
