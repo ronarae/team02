140be702132e1ef80fd7aebf137c06f30decf419
Shan Hu <shan.hu@hva.nl>
2020-10-04 17:54:43 +0200

    test

--- team02/src/app/components/events/overview4/overview4.component.ts
+++ team02/src/app/components/events/overview4/overview4.component.ts
@@ -31,7 +31,7 @@ export class Overview4Component implements OnInit {
   onSelect(eId:number) {
     //activate the details page for the given a-event Id
     this.router.navigate([eId], {relativeTo: this.activatedRoute});
-    console.log("ID: " + eId);
+  console.log("ID: " + eId);
   }
 
 }
