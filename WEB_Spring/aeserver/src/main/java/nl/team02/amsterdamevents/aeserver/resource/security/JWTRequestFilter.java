9de1e48207afcc4218b09e0eeea20354d8681779
ronaraederomasrieza <rona.rieza2393@gmail.com>
2020-12-04 15:39:07 +0100

    4.3.2 AB

--- WEB_Spring/aeserver/src/main/java/nl/team02/amsterdamevents/aeserver/resource/security/JWTRequestFilter.java
+++ WEB_Spring/aeserver/src/main/java/nl/team02/amsterdamevents/aeserver/resource/security/JWTRequestFilter.java
@@ -1,12 +1,67 @@
 package nl.team02.amsterdamevents.aeserver.resource.security;
 
+import nl.team02.amsterdamevents.aeserver.resource.exception.AuthenticationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
 
 /**
  * A filter used to check if a secured request has a valid token
  */
 @Component
-public class JWTRequestFilter {
+public class JWTRequestFilter extends OncePerRequestFilter {
+    //path prefixes that will be protected by the authentication
+    private static final Set<String> SECURED_PATHS =
+            Set.of("/aevents", "registrations", "/users");
+
+    private String passPhrase;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String servletPath = request.getServletPath();
+
+        if (HttpMethod.OPTIONS.matches(request.getMethod()) ||
+        SECURED_PATHS.stream().noneMatch(servletPath::startsWith)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        JWToken jwToken = null;
+        String encryptedToken = null;
+
+        //get encripted token string from authorization request header
+        encryptedToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (encryptedToken != null){
+            // remove bearer token prefix if used
+            encryptedToken = encryptedToken.replace("Bearer ", "");
+
+            //decode token
+            jwToken = JWToken.decode(encryptedToken, this.passPhrase);
+        }
+
+        //validate the token
+        if(jwToken == null){
+            throw new AuthenticationException("You need to login first");
+        }
+
+        // pass-on the token info as an attribute for the request
+        request.setAttribute(JWToken.JWT_ATTRIBUTE_NAME,jwToken);
+
+        filterChain.doFilter(request, response);
+    }
+
+
+
 
 }
 
--- WEB_Spring/aeserver/src/main/java/nl/team02/amsterdamevents/aeserver/resource/security/JWToken.java
+++ WEB_Spring/aeserver/src/main/java/nl/team02/amsterdamevents/aeserver/resource/security/JWToken.java
@@ -1,28 +1,34 @@
 package nl.team02.amsterdamevents.aeserver.resource.security;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 
 import java.security.Key;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JWToken {
+
     private static final String JWT_USERNAME_CLAIM = "sub";
     private static final String JWT_USERID_CLAIM = "id";
     private static final String JWT_ADMIN_CLAIM = "admin";
+    public static final String JWT_ATTRIBUTE_NAME  = "tokenInfo";
 
     private String userName = null;
     private Long userId = null;
     private boolean admin = false;
+
+    public JWToken() {}
+
+    public JWToken(String userName, Long userId, boolean admin) {
+        this.userName = userName;
+        this.userId = userId;
+        this.admin = admin;
+    }
 
     public String encode(String issuer, String passPhrase, int expiration) {
         Key key = getKey(passPhrase);
@@ -40,14 +46,39 @@ public class JWToken {
                 return token;
     }
 
+
+    public static JWToken decode(String token, String passPhrase) {
+        try{
+            //validate the token
+            Key key  = getKey(passPhrase);
+            Jws<Claims> jws = Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+            Claims claims = jws.getBody();
+
+            JWToken jwToken = new JWToken(
+                    claims.get(JWT_USERNAME_CLAIM).toString(),
+                    Long.valueOf(claims.get(JWT_USERID_CLAIM).toString()),
+                    (boolean) claims.get(JWT_ADMIN_CLAIM)
+            );
+
+            return jwToken;
+
+        } catch (ExpiredJwtException | MalformedJwtException |
+                UnsupportedJwtException | IllegalArgumentException e){
+            return null;
+        }
+    }
+
     /**
      * Get the secret key
      * @param passPhrase
      * @return secret key
      */
-    private Key getKey(String passPhrase) {
+    private static Key getKey(String passPhrase) {
         byte hmacKey[] = passPhrase.getBytes(StandardCharsets.UTF_8);
         Key key = new SecretKeySpec(hmacKey, SignatureAlgorithm.HS512.getJcaName());
         return key;
     }
+
+
+
 }
