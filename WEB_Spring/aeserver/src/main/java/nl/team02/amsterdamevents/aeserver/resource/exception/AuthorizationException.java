c414082d9b7d77ccc65d28a25c6368797185a6c6
ronaraederomasrieza <rona.rieza2393@gmail.com>
2020-12-10 12:20:30 +0100

    small change

--- WEB_Spring/aeserver/src/main/java/nl/team02/amsterdamevents/aeserver/resource/exception/AuthorizationException.java
+++ WEB_Spring/aeserver/src/main/java/nl/team02/amsterdamevents/aeserver/resource/exception/AuthorizationException.java
@@ -3,7 +3,7 @@ package nl.team02.amsterdamevents.aeserver.resource.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class AuthorizationException extends RuntimeException {
     public AuthorizationException(String message) {
         super(message);
