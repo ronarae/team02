1ebfca103803c300e279b00b3c4ff672ce8c2c47
ronaraederomasrieza <rona.rieza2393@gmail.com>
2020-12-12 21:54:59 +0100

    sb header visitor fixed + begin C

--- WEB_Angular/team02/src/app/components/mainpage/nav-bar/nav-bar.component.ts
+++ WEB_Angular/team02/src/app/components/mainpage/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SessionSbService} from "../../../services/session.sb.service";
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sessionService: SessionSbService) { }
 
   ngOnInit(): void {
   }
